df36afadf17586731e720a85fa807ad8bcf51ca0
Harley Boss <harleyaboss@gmail.com>
2020-02-17 19:03:00 -0500

    First commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MaterialDesignTest {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        private DockPanel currentMetrics;
+        private DockPanel prevMetrics;
+
+        public MainWindow() {
+            InitializeComponent();
+            currentMetrics = currentMetricsDock;
+
+        }
+
+        public void ShowCurrentData(object sender, RoutedEventArgs e) {
+            savedData.Visibility = Visibility.Hidden;
+            currentData.Visibility = Visibility.Visible;
+        }
+
+        public void ShowSavedData(object sender, RoutedEventArgs e) {
+            currentData.Visibility = Visibility.Hidden;
+            savedData.Visibility = Visibility.Visible;
+        }
+
+        public void LogOut(object sender, RoutedEventArgs e) {
+
+        }
+    }
+}
