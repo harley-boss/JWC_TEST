e6d399bce3ea42f129b2710be3e6c79f5ed04adb
Harley Boss <harleyaboss@gmail.com>
2020-02-17 19:27:40 -0500

    Fixed problem for judith

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,13 +17,11 @@ namespace MaterialDesignTest {
         }
 
         public void ShowCurrentData(object sender, RoutedEventArgs e) {
-            savedData.Visibility = Visibility.Hidden;
-            currentData.Visibility = Visibility.Visible;
+
         }
 
         public void ShowSavedData(object sender, RoutedEventArgs e) {
-            currentData.Visibility = Visibility.Hidden;
-            savedData.Visibility = Visibility.Visible;
+
         }
 
         public void LogOut(object sender, RoutedEventArgs e) {
